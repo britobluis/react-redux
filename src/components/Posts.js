633c09e03b36b2ed24402399d6974831e74b4ec1
Luisb <luis.brito@siwgroup.com>
2019-12-30 13:19:05 -0300

    Changed name of Post compononent to Posts, created postActions, and modified Posts.js and postReducer to use postActions

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,20 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Posts extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-            posts: []
-        }
-    }     
-
-    componentWillMount() {
-        console.log('mounted');
-        fetch(`https://jsonplaceholder.typicode.com/posts`)
-            .then(res => res.json())
-            .then(data => this.setState({posts: data}))
-    }
 
     render() {
         const postItems = this.state.posts.map(post => (
