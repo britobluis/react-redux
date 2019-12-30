4fd49a6f5f874b389bd03ae06b6497363b29fe16
Luisb <luis.brito@siwgroup.com>
2019-12-30 13:28:12 -0300

    Modified Posts.js to get data using redux

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,6 +1,9 @@
 import { FETCH_POSTS, NEW_POST } from './types';
 
 export const fetchPosts = () => (dispatch) => { // Function inside function
+
+    console.log('fetching');
+    
     fetch(`https://jsonplaceholder.typicode.com/posts`)
         .then(res => res.json())
         .then(posts => dispatch({
